f9eed60b61af4a62b70e9bdb1873d81a45e3dbce
gaojupeng2017 <640237309@qq.com>
2020-03-31 10:17:59 +0800

    xin jia detail.html ye mian

--- mysite/lib/urls.py
+++ mysite/lib/urls.py
@@ -2,4 +2,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('lib/',views.index,name='index'),]
+urlpatterns = [
+                path('lib/',views.index,name='index'),
+                path('detail/',views.detail,name='detail') ,
+              ]
+
